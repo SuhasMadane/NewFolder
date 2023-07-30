4653e442437bf2391c3ade00c5b217f811b8d05f
Suhas Madane <madanesuhas9@gmail.com>
2023-07-30 20:11:39 +0530

    push

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:8-jdk-alpine3.9
+COPY Program.class .
+CMD java Program
---
+++ Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter Your name");
+		String s = sc.nextLine();
+		System.out.println("Your Name: " + s);
+	}
+}
